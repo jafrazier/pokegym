ae92dbe65391dab1ae295a4f740a84a3639881e9
jfrazier <johnfrazier1222@gmail.com>
2018-07-25 16:21:02 -0400

    trainer function started

--- library.js
+++ library.js
@@ -45,27 +45,51 @@ function get(name, pokemon) {
     if (this.readyState == 4 && this.status == 200) {
       var myObj = JSON.parse(this.responseText);
       console.log(myObj.name);
-    if (name == 'trainer1') {
+    if (name == 'john') {
       document.getElementById('pokeimg1').src = myObj.sprites.front_default;
       document.getElementById('name1').innerHTML = myObj.name;
       document.getElementById('hp1').innerHTML = myObj.stats[5].base_stat;
       document.getElementById('atk1').innerHTML = myObj.stats[4].base_stat;
       document.getElementById('def1').innerHTML = myObj.stats[3].base_stat;
-    } if (name == 'john') {
+      document.getElementById('abilities1').innerHTML = myObj.abilities[0].ability['name'];
+    } if (name == 'trainer2') {
       document.getElementById('pokeimg2').src = myObj.sprites.front_default;
       document.getElementById('name2').innerHTML = myObj.name;
       document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
       document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
       document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
+      document.getElementById('abilities2').innerHTML = myObj.abilities[0].ability['name'];
     } if (name == 'tranier3') {
       document.getElementById('pokeimg2').src = myObj.sprites.front_default;
       document.getElementById('name2').innerHTML = myObj.name;
       document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
       document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
       document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
+      document.getElementById('abilities3').innerHTML = myObj.abilities[0].ability['name'];
     }
     }
   };
   xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+ pokeNum +'/', true);
   xhttp.send();
+}
+
+
+function getTrainer1(){
+  document.getElementById('train1').classList.remove('hidden');
+  document.getElementById('train2').classList.add('hidden');
+  document.getElementById('train3').classList.add('hidden');
+}
+
+function getTrainer2(){
+  document.getElementById('train2').classList.remove('hidden');
+  document.getElementById('train1').classList.add('hidden');
+  document.getElementById('train3').classList.add('hidden');
+
+}
+
+function getTrainer3(){
+  document.getElementById('train3').classList.remove('hidden');
+  document.getElementById('train1').classList.add('hidden');
+  document.getElementById('train2').classList.add('hidden');
+
 }
