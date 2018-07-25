a84796db8fae4a7b70c8679f10e924edcb186cc0
jfrazier <johnfrazier1222@gmail.com>
2018-07-25 14:27:06 -0400

    main function work

--- library.js
+++ library.js
@@ -45,13 +45,25 @@ function get(name, pokemon) {
     if (this.readyState == 4 && this.status == 200) {
       var myObj = JSON.parse(this.responseText);
       console.log(myObj.name);
-      // if (name ==) {
-      //
-      // } if (name ==) {
-      //
-      // } if (name ==) {
-      //
-      // }
+    if (name == 'trainer1') {
+      document.getElementById('pokeimg1').src = myObj.sprites.front_default;
+      document.getElementById('name1').innerHTML = myObj.name;
+      document.getElementById('hp1').innerHTML = myObj.stats[5].base_stat;
+      document.getElementById('atk1').innerHTML = myObj.stats[4].base_stat;
+      document.getElementById('def1').innerHTML = myObj.stats[3].base_stat;
+    } if (name == 'john') {
+      document.getElementById('pokeimg2').src = myObj.sprites.front_default;
+      document.getElementById('name2').innerHTML = myObj.name;
+      document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
+      document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
+      document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
+    } if (name == 'tranier3') {
+      document.getElementById('pokeimg2').src = myObj.sprites.front_default;
+      document.getElementById('name2').innerHTML = myObj.name;
+      document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
+      document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
+      document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
+    }
     }
   };
   xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+ pokeNum +'/', true);
