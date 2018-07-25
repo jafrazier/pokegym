293c4a409617cca04a7366c874b6108503599734
jfrazier <johnfrazier1222@gmail.com>
2018-07-25 18:10:43 -0400

    updates

--- library.js
+++ library.js
@@ -1,41 +1,41 @@
 trainers = {
   john:{
     volcanion: {
-      number: '721'
+      number: '721',
+      description: 'Volcanion is a primarily maroon, quadruped Pokémon with blue and yellow markings. It has large, pointed ears with blue insides and bright blue eyes. Additionally, it has bushy yellow eyebrows and a yellow mandible. There are spike-protrusions on each cheek that extend downward over its lower jaw. Volcanion\'s body has two oval segments separated by a thinner, blue portion. The front portion the body is larger and has two large, arm-like cannons on the back. These cannons are usually held together, forming a ring.'
     },
     drapion: {
-      number: '452'
+      number: '452',
+      description: 'Drapion is a large, purple, scorpion-like Pokémon. Its eyes are a pointed shape, with blue-colored upper portions. Drapion has two small, pointed protrusions between its eyes, and one on the back on its head. Drapion also has protrusions on its jaws, two on the upper and three on the lower, with large, fang-like, white-colored features extending from its mouth'
+
     },
     doublade: {
       number: '680'
     }
   },
   rex:{
-    name: '',
-    pokemon1: {
-      number: ''
+    giratina: {
+      number: '487'
     },
-    pokemon2: {
-      number: ''
+    kartana: {
+      number: '798'
     },
-    pokemon3: {
-      number: ''
+    rufflet: {
+      number: '627'
     },
   },
   hassan:{
-    name: '',
-    pokemon1: {
-      number: ''
+    salandit: {
+      number: '757'
     },
-    pokemon2: {
-      number: ''
+    heatmor: {
+      number: '631'
     },
-    pokemon3: {
-      number: ''
+    talonflame: {
+      number: '663'
     },
   }
 }
-
 function get(name, pokemon) {
   var myName = trainers[name];
   var mypokemon = myName[pokemon];
@@ -52,19 +52,20 @@ function get(name, pokemon) {
       document.getElementById('atk1').innerHTML = myObj.stats[4].base_stat;
       document.getElementById('def1').innerHTML = myObj.stats[3].base_stat;
       document.getElementById('abilities1').innerHTML = myObj.abilities[0].ability['name'];
-    } if (name == 'trainer2') {
+      document.getElementById('description').innerHTML = mypokemon['description'] ;
+    } if (name == 'rex') {
       document.getElementById('pokeimg2').src = myObj.sprites.front_default;
       document.getElementById('name2').innerHTML = myObj.name;
       document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
       document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
       document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
       document.getElementById('abilities2').innerHTML = myObj.abilities[0].ability['name'];
-    } if (name == 'tranier3') {
-      document.getElementById('pokeimg2').src = myObj.sprites.front_default;
-      document.getElementById('name2').innerHTML = myObj.name;
-      document.getElementById('hp2').innerHTML = myObj.stats[5].base_stat;
-      document.getElementById('atk2').innerHTML = myObj.stats[4].base_stat;
-      document.getElementById('def2').innerHTML = myObj.stats[3].base_stat;
+    } if (name == 'hassan') {
+      document.getElementById('pokeimg3').src = myObj.sprites.front_default;
+      document.getElementById('name3').innerHTML = myObj.name;
+      document.getElementById('hp3').innerHTML = myObj.stats[5].base_stat;
+      document.getElementById('atk3').innerHTML = myObj.stats[4].base_stat;
+      document.getElementById('def3').innerHTML = myObj.stats[3].base_stat;
       document.getElementById('abilities3').innerHTML = myObj.abilities[0].ability['name'];
     }
     }
@@ -72,21 +73,17 @@ function get(name, pokemon) {
   xhttp.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+ pokeNum +'/', true);
   xhttp.send();
 }
-
-
 function getTrainer1(){
   document.getElementById('train1').classList.remove('hidden');
   document.getElementById('train2').classList.add('hidden');
   document.getElementById('train3').classList.add('hidden');
 }
-
 function getTrainer2(){
   document.getElementById('train2').classList.remove('hidden');
   document.getElementById('train1').classList.add('hidden');
   document.getElementById('train3').classList.add('hidden');
 
 }
-
 function getTrainer3(){
   document.getElementById('train3').classList.remove('hidden');
   document.getElementById('train1').classList.add('hidden');
